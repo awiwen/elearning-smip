5622fe6e068a6634d58ce2f6742f32784039fe85
awiwen <awinsurya@gmail.com>
2017-12-31 13:25:15 +0800

    komentar pengajar

--- application/controllers/ccrudmateri_p.php
+++ application/controllers/ccrudmateri_p.php
@@ -430,21 +430,21 @@ public function showmaterikomentar(){
 
      <div class="form-group">
          <label for="siswa">User</label><br>
-           <select id="id_login" style="font-size:20px" class="btn dropdown-toggle btn-default" name="id_login" required>
+           <text id="id_login" style="font-size:20px" class="form-control" name="id_login" required>
              <label for="id_login" class="error"></label>
-         <option value=''>---- PILIH LOGIN ----</option>
+         <!-- <option value=''> PILIH LOGIN </option> -->
         <?php
            $this->load->model('mcrudmateri');
           $query = $this->mcrudmateri->selectloginkomentar_p();
         foreach($query->result() as $row){
           if($row->username == $this->session->userdata('username')){
         ?>
-         <option value="<?=$row->login_id?>"> user name: <?=$row->username?></option>
+         <text value="<?=$row->login_id?>"> <?=$row->username?></text>
          <?php
          }
        }
          ?>
-         </select>
+         </text>
      </div>
 
      <div class="form-group">
