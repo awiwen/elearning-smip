8f32938a0528b030da2825190c61e68445c8d680
awiwen <awinsurya@gmail.com>
2018-01-05 11:48:17 +0800

    pengajar pengajar

--- application/controllers/ccrudpengajar_p.php
+++ application/controllers/ccrudpengajar_p.php
@@ -12,14 +12,15 @@ function showpengajar(){
    <table id="example1" class="table table-bordered table-striped">
           <thead>
             <tr>
-                <th width="3%">No</th>
-                <th width="5%">NIP</th>
-                <th width="10%">Nama</th>
-                <th width="10%">Jenis Kelamin</th>
-                <th width="5%">Tempat Lahir</th>
-                <th width="10%">Tanggal Lahir</th>
-                <th width="20%">Alamat</th>
-                <th width="5%">Status</th>
+              <th width="3%">No</th>
+              <th width="12%">NUPTK</th>
+              <th width="20%">Nama</th>
+              <th width="10%">Jenis Kelamin</th>
+              <!-- <th width="5%">Tempat Lahir</th>
+              <th width="10%">Tanggal Lahir</th> -->
+              <th width="20%">Alamat</th>
+              <th width="3%">Status</th>
+              <th width="5%">Opsi</th>
             </tr>
           </thead>
         <tbody>
@@ -30,14 +31,22 @@ function showpengajar(){
       foreach($query->result() as $row){
         ?>
       <tr>
-                <td><?php echo $i ?></td>
-                <td><?php echo $row->nip ?></td>
-                <td><?php echo $row->nama ?></td>
-                <td><?php echo $row->jenis_kelamin?></td>
-                <td><?php echo $row->tempat_lahir ?></td>
-                <td><?php echo $row->tgl_lahir ?></td>
-                <td><?php echo $row->alamat ?></td>
-                <td><?php echo $row->status_nama?></td>
+        <td><?php echo $i ?></td>
+        <td><?php echo $row->nuptk ?></td>
+        <td><?php echo $row->nama ?></td>
+        <td><?php echo $row->jenis_kelamin?></td>
+        <!-- <td><?php echo $row->tempat_lahir ?></td>
+        <td><?php echo $row->tgl_lahir ?></td> -->
+        <td><?php echo $row->alamat ?></td>
+        <td><?php echo $row->status_nama?></td>
+        <td>
+          <script>
+          function redirect(url){
+            location.href = url;
+          }
+          </script>
+          <button onclick="redirect('http://localhost/elearning-smip/index.php/cdetailpengajar/showdetailpengajar/<?= $row->pengajar_id?>')"
+            type="button" class="btn btn-primary btn-xs">Detail</button>
               </tr>
       <?php
       $i++;
